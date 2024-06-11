8665d07112d08ca422eaa565b4f009f3d53f0762
thebenoit <jerrybenoit@yahoo.com>
2024-06-11 16:00:38 -0400

    ajout de la video comme background

--- vite-project/src/components/HeroSection.vue
+++ vite-project/src/components/HeroSection.vue
@@ -1,5 +1,12 @@
 <template>
-  <div class="hero min-h-screen" style="background-image: url(https://img.daisyui.com/images/stock/photo-1507358522600-9f71e620c44e.jpg);">
+  <div class="hero min-h-screen overflow-hidden">
+    <video
+      class="absolute inset-0 w-full h-full object-cover"
+      src="@/assets/videos/mtlStreet.mp4"
+      autoplay
+      loop
+      muted
+    ></video>
     <div class="hero-overlay bg-opacity-60"></div>
     <div class="hero-content text-center text-neutral-content">
       <div class="max-w-md">
