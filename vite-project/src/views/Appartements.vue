fc1273ede32ccee690d3473b3083b6569c588b6a
FΣD <2063054@bdeb.qc.ca>
2024-06-17 23:59:41 -0400

    fixes

--- vite-project/src/views/Appartements.vue
+++ vite-project/src/views/Appartements.vue
@@ -7,22 +7,36 @@ import Footer from '../components/Footer.vue'
 // Initialize the `apparts` ref as an empty array
 const apparts = ref([]);
 
+const isRendered = ref(false)
+
 onMounted(async () => {
   // Await the result of `utils.get('appartments')` and assign it to `apparts.value`
   const response = await utils.get('appartments');
   apparts.value = response;
+  isRendered.value = true
 });
 </script>
 
 
 <template>
   
-  <div class=" flex w-screen  inset-0 bg-slate-200">
-    <div class>
-      <h1 class="">Vos Résultats</h1>
-
-      <div class="flex flex-wrap gap-5 justify-between">
-      
+  <div class="flex inset-0 bg-slate-500">
+    <div>
+      <div class="flex flex-wrap gap-5 justify-between mx-32 mt-24">
+        <div v-if="!isRendered" class="w-96 h-80 skeleton"></div>
+        <div v-if="!isRendered" class="w-96 h-80 skeleton"></div>
+        <div v-if="!isRendered" class="w-96 h-80 skeleton"></div>
+        <div v-if="!isRendered" class="w-96 h-80 skeleton"></div>
+        <div v-if="!isRendered" class="w-96 h-80 skeleton"></div>
+        <div v-if="!isRendered" class="w-96 h-80 skeleton"></div>
+        <div v-if="!isRendered" class="w-96 h-80 skeleton"></div>
+        <div v-if="!isRendered" class="w-96 h-80 skeleton"></div>
+        <div v-if="!isRendered" class="w-96 h-80 skeleton"></div>
+        <div v-if="!isRendered" class="w-96 h-80 skeleton"></div>
+        <div v-if="!isRendered" class="w-96 h-80 skeleton"></div>
+        <div v-if="!isRendered" class="w-96 h-80 skeleton"></div>
+        <div v-if="!isRendered" class="w-96 h-80 skeleton"></div>
+        
       
         <div v-for="appart in apparts" :key="appart.url">
           
@@ -37,10 +51,6 @@ onMounted(async () => {
         </div> 
       </div>
 
-    </div>
-
-    
-    
-   
+    </div>   
   </div>
 </template>
