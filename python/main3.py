a5722db96b973053f052a06b15a7c5ac7f119fa2
FΣD <2063054@bdeb.qc.ca>
2024-06-20 18:17:22 -0400

    Update main3.py

--- python/main3.py
+++ python/main3.py
@@ -1,13 +1,48 @@
-from playwright.sync_api import sync_playwright
+from seleniumwire import webdriver  # Import from seleniumwire
+from selenium.webdriver.common.by import By
 
-with sync_playwright() as p:
-    browser = p.chromium.launch()
-    page = browser.new_page()
-    page.on('request', lambda request: print(f'Request: {request.url}'))
-    page.on('response', lambda response: print(f'Response: {response.url}'))
-    page.goto(f"https://www.facebook.com/marketplace/montreal/search?minPrice=300&maxPrice=3000&minBedrooms=1&minBathrooms=1&minSqft=500&maxSqft=2000&daysSinceListed=7&query=2%20bedroom%20appartment&exact=false")
+from seleniumwire.utils import decode
+import json
+import time
+
+# Create a new instance of the Chrome driver
+driver = webdriver.Chrome()
+
+# Go to the Google home page
+driver.get(f"https://www.facebook.com/marketplace/montreal/search?minPrice=300&maxPrice=3000&minBedrooms=1&minBathrooms=1&minSqft=500&maxSqft=2000&daysSinceListed=7&query=2%20bedroom%20appartment&exact=false")
+
+time.sleep(5)
+
+try:
+    i = 0
 
     while True:
-        page.keyboard.down('End')
+        # Scroll down to bottom
+        driver.execute_script("window.scrollTo(0, document.body.scrollHeight);")
+
+        # Wait to load page
+        time.sleep(2)
+
+        # Calculate new scroll height and compare with last scroll height
+        new_height = driver.execute_script("return document.body.scrollHeight")
+        if new_height == last_height:
+            break
+
+        last_height = new_height
+
+        i += 1
+
+        if i == 10:
+            break
+
+except Exception as e:
+    print(e)
 
 
+# Access requests via the `requests` attribute
+for request in driver.requests:
+    if request.response:
+        if "graphql" in request.url:
+            body = decode(request.response.body, request.response.headers.get('Content-Encoding', 'identity'))
+            body = json.loads(body)
+            print(body)
