b5b38bd27cbe1ae1cdbd2802d0d850e26266fe87
FΣD <2063054@bdeb.qc.ca>
2024-07-25 00:03:44 -0400

    fix home

--- vite-project/src/views/Home.vue
+++ vite-project/src/views/Home.vue
@@ -17,8 +17,8 @@ function handleSubmitButton() {
 <template>
   
   <div class="hero overflow-hidden h-screen w-full">
-    <img class="inset-0 object-cover h-screen w-full" src="@/assets/images/pexels-5.jpg"></img>
-    <div class="hero-overlay bg-opacity-65 inset-0"></div>
+    <img class="inset-0 object-cover h-screen w-full" src="@/assets/images/main1.jpg"></img>
+    <div class="inset-0"></div>
     <div class="hero-content text-center text-neutral-content relative z-10 flex items-center justify-center w-full ">
       <div class="">
         <h1 class="mb-5 text-4xl font-bold ">À la recherche d'un appartement abordable?</h1>
@@ -28,6 +28,4 @@ function handleSubmitButton() {
   </div>
 </template>
 
-<style>
-
-</style>
+<style></style>
