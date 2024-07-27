951f5179d1a4d8987674936b8f4f41178bcadb25
thebenoit <jerrybenoit@yahoo.com>
2024-07-27 19:36:45 -0400

    multi-range input style finished

---
+++ vite-project/src/components/ui/input/multirange-input/multiRangeInput.vue
@@ -0,0 +1,100 @@
+<template>
+    <div class="flex justify-center items-center">
+      <div class="relative max-w-xl w-full">
+        <div>
+          <input
+            type="range"
+            step="100"
+            :min="min"
+            :max="max"
+            @input="mintrigger"
+            v-model="minprice"
+            class="absolute pointer-events-none appearance-none z-20 h-2 w-full opacity-0 cursor-pointer"
+          />
+  
+          <input
+            type="range"
+            step="100"
+            :min="min"
+            :max="max"
+            @input="maxtrigger"
+            v-model="maxprice"
+            class="absolute pointer-events-none appearance-none z-20 h-2 w-full opacity-0 cursor-pointer"
+          />
+  
+          <div class="relative z-10 h-2">
+            <div class="absolute z-10 left-0 right-0 bottom-0 top-0 rounded-md bg-gray-200"></div>
+            <div
+              class="absolute z-20 top-0 bottom-0 rounded-md bg-blue-main"
+              :style="{ right: maxthumb + '%', left: minthumb + '%' }"
+            ></div>
+            <div
+              class="absolute z-30 w-6 h-6 top-0 left-0 bg-blue-main rounded-full -mt-2 -ml-1"
+              :style="{ left: minthumb + '%' }"
+            ></div>
+            <div
+              class="absolute z-30 w-6 h-6 top-0 right-0 bg-blue-main rounded-full -mt-2 -mr-3"
+              :style="{ right: maxthumb + '%' }"
+            ></div>
+          </div>
+        </div>
+  
+        <div class="flex justify-between items-center py-5">
+          <div>
+            <input
+              type="text"
+              maxlength="5"
+              @input="mintrigger"
+              v-model="minprice"
+              class="px-3 py-2 border border-gray-200 rounded w-24 text-center"
+            />
+          </div>
+          <div>
+            <input
+              type="text"
+              maxlength="5"
+              @input="maxtrigger"
+              v-model="maxprice"
+              class="px-3 py-2 border border-gray-200 rounded w-24 text-center"
+            />
+          </div>
+        </div>
+      </div>
+    </div>
+  </template>
+  
+  <script setup>
+  import { ref, onMounted } from 'vue';
+  
+  const minprice = ref(1000);
+  const maxprice = ref(7000);
+  const min = ref(100);
+  const max = ref(10000);
+  const minthumb = ref(0);
+  const maxthumb = ref(0);
+  
+  const mintrigger = () => {
+    minprice.value = Math.min(minprice.value, maxprice.value - 500);
+    minthumb.value = ((minprice.value - min.value) / (max.value - min.value)) * 100;
+  };
+  
+  const maxtrigger = () => {
+    maxprice.value = Math.max(maxprice.value, minprice.value + 500);
+    maxthumb.value = 100 - (((maxprice.value - min.value) / (max.value - min.value)) * 100);
+  };
+  
+  onMounted(() => {
+    mintrigger();
+    maxtrigger();
+  });
+  </script>
+  
+  <style scoped>
+  input[type="range"]::-webkit-slider-thumb {
+    pointer-events: all;
+    width: 24px;
+    height: 24px;
+    -webkit-appearance: none;
+  }
+  </style>
+  
--- vite-project/src/views/PreferenceSurvey/Preferences.vue
+++ vite-project/src/views/PreferenceSurvey/Preferences.vue
@@ -1,14 +1,60 @@
 <script setup>
-import utils from '../../utils/utils.js';
+import utils from "../../utils/utils.js";
+import MultiSlider from "../../components/ui/input/multirange-input/multiRangeInput.vue"
+import {ref} from 'vue'
+
+const minValue = ref(0);
+const maxValue = ref(100);
 </script>
 
 <template>
-  <div class="hero overflow-hidden min-h-screen flex justify-around w-full">
-    <ul class="steps">
+  <div class="h-screen">
+    <section>
+      <h1 class="text-xl text-blue-main text-center">
+        Comprendre vos préférences...
+      </h1>
+    </section>
+
+    <section>
+      <div class="bg-gray-200 m-10 border-2 rounded-md  shadow-lg">
+        <h2 class="m-2 text-gray-500 text-center">
+          ces réponses nous permetra de selectionner les appartements qui vous
+          correspondent le mieux
+        </h2>
+      </div>
+
+      <div>
+        <div class="card card-body border-2 w-full">
+          <div class="card card-body border-2 m-5 shadow-lg rounded-lg">
+            <h1 class="text-blue-main text-center mb-5">Combien de chambre?</h1>
+            <div class=" ">
+              <ul class="flex flex-row justify-center ">
+                <li><p class="btn btn-active m-2">1</p></li>
+                <li><p class="btn btn-active m-2">2</p></li>
+                <li><p class="btn btn-active m-2">3</p></li>
+                <li><p class="btn btn-active m-2">4</p></li>
+                <li><p class="btn btn-active m-2">5+</p></li>
+              </ul>
+            </div>
+            
+          </div>
+          <div class="card card-body border-2 m-5 shadow-lg rounded-lg">
+            <h1 class="text-center mb-5 text-blue-main">Votre Budget</h1>
+
+            <MultiSlider :min="0" :max="100" @update:minValue="minValue = $event" @update:maxValue="maxValue = $event"></MultiSlider>
+          </div>
+          
+        </div>
+      </div>
+    </section>
+
+    <div class="hero overflow-hidden flex flex-col justify-end w-full">
+      <ul class="steps">
         <li class="step step-primary">Register</li>
         <li class="step step-primary">Choose plan</li>
         <li class="step step-primary">Purchase</li>
         <li class="step step-primary">Receive Product</li>
-    </ul>
+      </ul>
+    </div>
   </div>
 </template>
