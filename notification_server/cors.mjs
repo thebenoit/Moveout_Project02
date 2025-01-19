cb191c9338d4e5569d6a5262f28aeb13f9a24acf
thebenoit <jerrybenoit@yahoo.com>
2025-01-18 22:19:33 -0500

    Quelques modif

--- notification_server/cors.mjs
+++ notification_server/cors.mjs
@@ -1,38 +1,37 @@
-import http from "http";
-import url from "url";
-import httpStatus from "./httpStatus.mjs";
-import logger from "./logger.js";
+// import http from "http";
+// import url from "url";
+// import httpStatus from "./httpStatus.mjs";
+// // import logger from "./logger.js";
 
+// async function CORS_preflignt(req, res) {
+//     logger.log("info", "ins_utilisateur --OPTION");
+//     res.setHeader("Access-Control-Allow-Origin", "*");
+//     res.setHeader(
+//       "Access-Control-Request-Method",
+//       "DELETE, GET, HEAD, OPTIONS, PATCH, POST, PUT"
+//     );
+//     res.setHeader(
+//       "Access-Control-Allow-Methods",
+//       "DELETE, GET, HEAD, OPTIONS, PATCH, POST, PUT"
+//     );
+//     res.statusCode = 200;
+//     res.end();
+//   }
 
-async function CORS_preflignt(req, res) {
-    logger.log("info", "ins_utilisateur --OPTION");
-    res.setHeader("Access-Control-Allow-Origin", "*");
-    res.setHeader(
-      "Access-Control-Request-Method",
-      "DELETE, GET, HEAD, OPTIONS, PATCH, POST, PUT"
-    );
-    res.setHeader(
-      "Access-Control-Allow-Methods",
-      "DELETE, GET, HEAD, OPTIONS, PATCH, POST, PUT"
-    );
-    res.statusCode = 200;
-    res.end();
-  }
-  
-  async function setCORSHeaders(req, res) {
-    logger.log("info", "setCORSHeaders --OPTION");
-    res.setHeader("Access-Control-Allow-Origin", "*");
-    res.setHeader(
-      "Access-Control-Allow-Methods",
-      "DELETE, GET, HEAD, OPTIONS, PATCH, POST, PUT"
-    );
-    res.setHeader(
-      "Access-Control-Allow-Headers",
-      "Origin, X-Requested-With, Content-Type, Accept"
-    );
-  }
+//   async function setCORSHeaders(req, res) {
+//     logger.log("info", "setCORSHeaders --OPTION");
+//     res.setHeader("Access-Control-Allow-Origin", "*");
+//     res.setHeader(
+//       "Access-Control-Allow-Methods",
+//       "DELETE, GET, HEAD, OPTIONS, PATCH, POST, PUT"
+//     );
+//     res.setHeader(
+//       "Access-Control-Allow-Headers",
+//       "Origin, X-Requested-With, Content-Type, Accept"
+//     );
+//   }
 
-  export {
-    setCORSHeaders,
-    CORS_preflignt
-  }
+//   export {
+//     setCORSHeaders,
+//     CORS_preflignt
+//   }
--- notification_server/logMessages.js
+++ notification_server/logMessages.js
@@ -1,23 +1,23 @@
-import http from "http";
-import url from "url";
-import httpStatus from "./httpStatus.mjs";
-import logger from "./logger.js";
-import {
-    setCORSHeaders,
-    CORS_preflignt
-} from "./cors.mjs"
+// import http from "http";
+// import url from "url";
+// import httpStatus from "./httpStatus.mjs";
+// import logger from "./logger.js";
+// import {
+//     setCORSHeaders,
+//     CORS_preflignt
+// } from "./cors.mjs"
 
 
-async function envoyer_reponse(req, res, reponse, httpStatusCode) {
-    res.statusCode = httpStatusCode;
-    logger.log("info", "DEBUT >>> envoyer_reponse");
-    logger.log("info", `HTTP Status Code: ${httpStatusCode}`);
-    res.statusCode = httpStatusCode;
-    setCORSHeaders(req, res);
-    res.setHeader("Content-Type", "text/plain");
-    res.end(reponse);
-  }
+// async function envoyer_reponse(req, res, reponse, httpStatusCode) {
+//     res.statusCode = httpStatusCode;
+//     logger.log("info", "DEBUT >>> envoyer_reponse");
+//     logger.log("info", `HTTP Status Code: ${httpStatusCode}`);
+//     res.statusCode = httpStatusCode;
+//     setCORSHeaders(req, res);
+//     res.setHeader("Content-Type", "text/plain");
+//     res.end(reponse);
+//   }
 
-  export {
-    envoyer_reponse
-  }
+//   export {
+//     envoyer_reponse
+//   }
