4490958b948fafcad92cf6d4715ae282ce16e969
thebenoit <jerrybenoit@yahoo.com>
2025-01-25 23:04:08 -0500

    finis setup exchanges

--- notification_server/config/rabbitmq.js
+++ notification_server/config/rabbitmq.js
@@ -6,6 +6,39 @@ class RabbitMQConnection {
     this.channels = new Map(); //pour stocker plusieurs channels
     this.url = process.env.RABBITMQ_URI;
     this.isReconnecting = false;
+
+    this.exchange = {
+      notification: {
+        name: "notification.exchange",
+        type: "direct",
+        options: { durable: true },
+      },
+      dlx: {
+        name: "notification.dlx",
+        type: "direct",
+        options: { durable: true },
+      },
+    };
+
+    this.queues = {
+      notification: {
+        name: "notification.queue",
+        options: {
+          durable: true,
+          arguments: {
+            "x-dead-letter-exchange": "notification.dlx",
+            "x-dead-letter-routing-key": "notification.dead",
+            "x-message-ttl": 30000, // 30 secondes
+            "x-max-length": 1000, // Limite de 10000 messages
+            "x-max-priority": 10, //support des priorités
+          },
+        },
+      },
+      dead: {
+        name: "notification.dead.queue",
+        options: { durable: true },
+      },
+    };
   }
 
   async connect() {
@@ -29,6 +62,7 @@ class RabbitMQConnection {
           );
           this.reconnect();
         });
+        await this.setupExchangesAndQueues();
       }
       console.log("✅ Connecté à RabbitMQ");
       return this.connection;
@@ -38,6 +72,56 @@ class RabbitMQConnection {
       setTimeout(() => this.connect(), 5000);
     }
   }
+
+  async setupExchangesAndQueues() {
+    try {
+      const channel = await this.createChannel("setup");
+
+      //Création des exchanges
+      await channel.assertExchange(
+        this.exchanges.notification.name,
+        this.exchanges.notification.type,
+        this.exchanges.notification.options
+      );
+
+      await channel.assertExchange(
+        this.exchanges.dlx.name,
+        this.exchanges.dlx.type,
+        this.exchanges.dlx.options
+      );
+
+      // Création des queues
+      await channel.assertQueue(
+        this.queues.notification.name,
+        this.queues.notification.options
+      );
+
+      await channel.assertQueue(
+        this.queues.dead.name,
+        this.queues.dead,
+        options
+      );
+
+      //Binding des queues aux exchanges
+      await channel.bindQueue(
+        this.queues.notification.name,
+        this.exchanges.notification.name,
+        "notification.routing"
+      );
+
+      await channel.bindQueue(
+        this.queues.dead.name,
+        this.exchanges.dlx.name,
+        "notification.dead"
+      );
+
+      console.log("✅ Exchanges et queues créés et liés");
+    } catch (err) {
+      console.error("Erreur de configuration des exchanges et queues", error);
+      throw error;
+    }
+  }
+
   async createChannel(channelId) {
     try {
       if (!this.connection) {
