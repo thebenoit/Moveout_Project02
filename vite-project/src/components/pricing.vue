7b15ae24ce629f931d70360b7cfbd750421bf17c
thebenoit <jerrybenoit@yahoo.com>
2025-03-24 23:08:48 -0400

    Pret à etre lunch

--- vite-project/src/components/pricing.vue
+++ vite-project/src/components/pricing.vue
@@ -8,14 +8,14 @@ const btn_continuer_gratuitement = () => {
   router.push("/listings");
 };
 //lien pour accéder au portail stripe
-const customerPortalLink = ref("open ");
+const customerPortalLink = ref("https://buy.stripe.com/3cseXl7mn3nmcNy28a ");
 </script>
 
 <template>
   <div
     class="mt-10 p-2 flex-col sm:flex sm:flex-row justify-center sm:space-x-10 max-w-sm sm:max-w-max sm:w-full mx-auto sm:space-y-0 space-y-10"
   >
-    <div
+    <!-- <div
       class="flex flex-col border-black-600 border-2 shadow-black-600 shadow-lg rounded-3xl bg-white ring-1 ring-black/10"
     >
       <div class="p-8 pb-0 sm:p-10 sm:pb-0">
@@ -97,12 +97,12 @@ const customerPortalLink = ref("open ");
               </p>
             </li>
           </ul>
-          <!-- <div class="mt-8">
+          <div class="mt-8">
                 <a href="/login" class="inline-block w-full rounded-lg bg-gray-900 px-4 py-4 text-center text-sm font-semibold leading-5 text-white shadow-md hover:bg-white hover:text-gray-900 hover:ring hover:ring-gray-900" aria-describedby="tier-team">Get started today</a>
-              </div> -->
+              </div>
         </div>
       </div>
-    </div>
+    </div> -->
     <div
       class="flex flex-col border-blue-main border-2 shadow-blue-main shadow-lg rounded-3xl bg-white ring-1 ring-black/10"
     >
@@ -111,20 +111,20 @@ const customerPortalLink = ref("open ");
           class="text-xl leading-8 tracking-tight text-blue-main"
           id="tier-team"
         >
-          Pro
+          Moveout Bot
         </h3>
         <h3
           class="text-2xl font-semibold leading-8 tracking-tight text-blue-main mb-5"
           id="tier-team"
         >
-          9.99$/mois
+          29$/mois
         </h3>
         <a
           :href="customerPortalLink"
           target="_blank"
           class="inline-block bg-blue-main text-white mb-5 px-4 py-2 rounded-full w-full text-center"
         >
-          Aller Pro
+        Continuer avec Moveout Bot
         </a>
       </div>
       <div class="flex flex-1 flex-col p-2 pt-0">
@@ -151,7 +151,7 @@ const customerPortalLink = ref("open ");
                 </svg>
               </div>
               <p class="ml-3 text-sm leading-6 text-gray-600">
-                Tout du plan Gratuit
+                Fini les annonces obsolètes
               </p>
             </li>
 
@@ -218,7 +218,7 @@ const customerPortalLink = ref("open ");
                 </svg>
               </div>
               <p class="ml-3 text-sm leading-6 text-gray-600">
-                +5 Appartements par jours 🏡
+                Communique en votre langue préférée
               </p>
             </li>
             <li class="flex items-start">
